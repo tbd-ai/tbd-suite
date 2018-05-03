8cfef4c61cb5bebd021a7a7d9c65112c49618507
Hongyu Zhu <serailhydra@gmail.com>
2018-05-03 17:17:54 -0400

    Update resnet-imagenet.sh

--- ImageClassification-Resnet_50/MXNet/scripts/resnet-imagenet.sh
+++ ImageClassification-Resnet_50/MXNet/scripts/resnet-imagenet.sh
@@ -16,7 +16,7 @@ elif [ "$1" = "--profile-fp32" ]
 then
 	mkdir -p measurements
 	PREFIX="/usr/local/cuda/bin/nvprof --profile-from-start off \
-		--export-profile measurements/resnet50-mxnet.nvvp -f \
+		--export-profile measurements/resnet50-mxnet-fp32.nvvp -f \
 		--metrics single_precision_fu_utilization"
 	SUFFIX=" --nvprof-on True"
 
