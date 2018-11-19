95a021493f31bd253709eb0a111c7b6553632840
Jack Zheng <bojian.zheng@mail.utoronto.ca>
2018-11-19 10:43:04 -0500

    Update the IWSLT15 dataset download link.

--- MachineTranslation-Seq2Seq/Sockeye/dataset/download_iwslt15_en-vi.sh
+++ MachineTranslation-Seq2Seq/Sockeye/dataset/download_iwslt15_en-vi.sh
@@ -2,5 +2,5 @@
 
 SOCKEYE_ROOT=$(cd $(dirname $0) && pwd)/..
 
-cd $SOCKEYE_ROOT/dataset; wget https://www.cs.toronto.edu/~bojian/Downloads/iwslt15_en-vi.tar.gz; \
+cd $SOCKEYE_ROOT/dataset; wget http://www.cs.toronto.edu/~bojian/Downloads/NMT/iwslt15-vi_en.tar.gz; \
 	tar xvzf iwslt15_en-vi.tar.gz; rm -f iwslt15_en-vi.tar.gz
