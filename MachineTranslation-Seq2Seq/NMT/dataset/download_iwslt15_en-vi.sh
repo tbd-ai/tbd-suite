6bfdf129c18c97ed2ee841fe339499549cd942f2
Jack Zheng <bojian.zheng@mail.utoronto.ca>
2018-11-19 10:42:20 -0500

    Update the IWSLT 15 download link.

--- MachineTranslation-Seq2Seq/NMT/dataset/download_iwslt15_en-vi.sh
+++ MachineTranslation-Seq2Seq/NMT/dataset/download_iwslt15_en-vi.sh
@@ -2,5 +2,5 @@
 
 NMT_ROOT=$(cd $(dirname $0) && pwd)/..
 
-cd $NMT_ROOT/dataset; wget https://www.cs.toronto.edu/~bojian/Downloads/iwslt15_en-vi.tar.gz; \
+cd $NMT_ROOT/dataset; wget http://www.cs.toronto.edu/~bojian/Downloads/NMT/iwslt15-vi_en.tar.gz; \
 	tar xvzf iwslt15_en-vi.tar.gz; rm -f iwslt15_en-vi.tar.gz
