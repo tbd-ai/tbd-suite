e9a35f887a7457308c873d44fe0c6393ab393af9
dingtaoliu <danny.dingtao.liu@gmail.com>
2019-12-03 22:27:33 -0500

    switched from glob.glob to Path.rglob for file names

--- LanguageModeling-BERT/TensorFlow/scripts/prepare_wiki_data.sh
+++ LanguageModeling-BERT/TensorFlow/scripts/prepare_wiki_data.sh
@@ -1,7 +1,10 @@
+cd ..
+DIR=$(pwd)
+
 echo "Extracting wiki data..."
-python3 ./pretrain/WikiExtractor.py -o ../dataset/pretrain/extracted/ ../dataset/pretrain/download/wiki_en.xml 
+python3 $DIR"/scripts/pretrain/WikiExtractor.py" -o $DIR"/dataset/pretrain/extracted/" $DIR"/dataset/pretrain/download/wiki_en.xml" 
 echo "done!"
 
 echo "Formatting wiki data..."
-python3 ./pretrain/format_wiki_data.py 
+python3 $DIR"/scripts/pretrain/format_wiki_data.py" $DIR"/dataset/pretrain/extracted" $DIR"/dataset/pretrain/wiki/wikicorpus_en_one_article_per_line.txt"
 echo "done!"
--- LanguageModeling-BERT/TensorFlow/scripts/pretrain/format_wiki_data.py
+++ LanguageModeling-BERT/TensorFlow/scripts/pretrain/format_wiki_data.py
@@ -13,6 +13,8 @@
 
 import glob
 import os
+import sys
+from pathlib import Path
 
 class WikicorpusTextFormatting:
     def __init__(self, wiki_path, output_filename, recursive = False):
@@ -23,20 +25,15 @@ class WikicorpusTextFormatting:
 
     # This puts one article per line
     def merge(self):
-        count = 0
         with open(self.output_filename, mode='w', newline='\n') as ofile:
-            #for dirname in glob.glob(self.wiki_path + '/*/', recursive=False):
-            for filename in glob.glob(self.wiki_path + 'wiki_*', recursive=self.recursive):
+            for filename in Path(self.wiki_path).rglob('wiki_*'):
                 print(filename)
                 article_lines = []
                 article_open = False
 
                 with open(filename, mode='r', newline='\n') as file:
-                    print("starting formating")
+                    print("Started formating {}".format(filename))
                     for line in file:
-                        if count == 1000:
-                            print("formatted 1000 documents, terminating")
-                            break
                         if '<doc id=' in line:
                             article_open = True
                         elif '</doc>' in line:
@@ -46,14 +43,12 @@ class WikicorpusTextFormatting:
                                     ofile.write(oline.rstrip() + " ")
                             ofile.write("\n\n")
                             article_lines = []
-                            count += 1
-                            print("finished processing {} documents".format(count))
                         else:
                             if article_open:
                                 article_lines.append(line)
 
 if __name__ == "__main__":
-    wiki_path = "../../dataset/pretrain/extracted/"
-    output_filename = "../../dataset/pretrain/wiki/wikicorpus_en_one_article_per_line.txt"
+    wiki_path = sys.argv[1]
+    output_filename = sys.argv[2]
     wiki_formatter = WikicorpusTextFormatting(wiki_path, output_filename, recursive=True)
     wiki_formatter.merge()
