940bc3676563f78edb1bd36181e62f0b7261e8e4
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-16 08:17:51 +0000

    Adding tarfile member sanitization to extractall()

--- SpeechRecognition-DeepSpeech2/pytorch/dataset/librispeech.py
+++ SpeechRecognition-DeepSpeech2/pytorch/dataset/librispeech.py
@@ -72,7 +72,26 @@ def main():
                 wget.download(url, split_dir)
             print("Unpacking {}...".format(filename))
             with tarfile.open(target_filename) as tar:
-                tar.extractall(split_dir)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tar, split_dir)
             os.remove(target_filename)
             print("Converting flac files to wav and extracting transcripts...")
             assert os.path.exists(extracted_dir), "Archive {} was not properly uncompressed.".format(filename)
